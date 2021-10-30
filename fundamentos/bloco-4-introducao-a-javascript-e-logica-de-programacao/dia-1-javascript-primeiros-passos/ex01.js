18ef9e8cbb1b53a0f5ab4b258d76f76373353bbb
Caio-Veloso <caio.veloso@gmail.com>
2021-10-30 20:38:33 -0300

    Questao 1

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/ex01.js
@@ -0,0 +1,16 @@
+/* Nos exercícios de hoje, você vai fazer quinze pequenos programas. Todos trabalham a lógica condicional ( if/else e switch/case ), operadores aritméticos ( + , - , * , / , % ) e operadores lógicos ( > , < , && , || ). Para que consiga executar seus códigos recomendamos que utilize a extensão Code Runner , você pode ver mais sobre ela no conteúdo que fizemos sobre o uso do VS Code .
+1)  Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b , definidas no começo com os valores que serão operados. Faça programas para:
+Adição (a + b)
+Subtração (a - b)
+Multiplicação (a * b)
+Divisão (a / b)
+Módulo (a % b) */
+
+const a = 4
+const b = 7
+
+console.log(`A soma de a e com b é ${a + b}`);
+console.log(`A subtração de a e com b é ${a - b}`);
+console.log(`A multiplicação de a e com b é ${a * b}`);
+console.log(`A divisão de a e com b é ${a / b}`);
+console.log(`O módulo  de a e com b é ${a % b}`);
