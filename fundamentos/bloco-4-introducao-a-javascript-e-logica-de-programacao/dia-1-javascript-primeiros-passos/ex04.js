3427bfedeb2a9625b5cf2d850fb0829baa99ce48
Caio-Veloso <caio.veloso@gmail.com>
2021-10-31 08:10:27 -0300

    Exercicio 4

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/ex04.js
@@ -0,0 +1,13 @@
+/* 4) Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário. */
+
+const value = -45;
+
+if(value > 0) {
+  console.log("positive");
+}
+else if(value === 0) {
+  console.log("zero");
+}
+else {
+  console.log("negative");
+}
