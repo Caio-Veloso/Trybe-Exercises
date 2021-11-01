eb58ac771b4d2704ee0cd234b1c8126a7bc7bfd3
Caio-Veloso <caio.veloso@gmail.com>
2021-11-01 13:07:13 -0300

    Questao 4

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/ex04.js
@@ -0,0 +1,15 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+let avg =0;
+
+for(let index =0; index < numbers.length; index +=1){
+  sum += numbers[index];
+  avg = sum / numbers.length;
+  console.log(avg);
+}
+if(avg > 20) {
+  console.log("Valor maior do que 20");
+} 
+else{
+  console.log("valor menor do que 20");
+}
