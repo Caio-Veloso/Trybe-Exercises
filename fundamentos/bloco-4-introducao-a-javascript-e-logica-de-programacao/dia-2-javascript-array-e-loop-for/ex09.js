18adf13afafadc695612b0a99f0584a0de4793f8
Caio-Veloso <caio.veloso@gmail.com>
2021-11-01 18:53:25 -0300

    Questao 9

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/ex09.js
@@ -0,0 +1,16 @@
+let numbers = [];
+
+for (let index = 1; index <= 25; index++) {
+  numbers.push(index);
+
+  
+}
+
+console.log(numbers);
+
+
+for (let index = 0; index < numbers.length; index++) {
+  console.log(numbers[index]/2);  
+  
+}
+
